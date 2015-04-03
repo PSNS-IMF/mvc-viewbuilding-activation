cade5d0938dd1bce1b939e0c7b066f9d48135b21
Jeff Purcell <jeffrey.a.purcell@navy.mil>
2015-04-03 12:53:27 -0700

    Initial commit.

---
+++ Psns.Common.Mvc.ViewBuilding.Activation/ViewBuildingActivator.cs
@@ -0,0 +1,37 @@
+﻿[assembly: WebActivatorEx.PostApplicationStartMethod(typeof(Psns.Common.Mvc.ViewBuilding.Activation.App_Start.ViewBuildingActivator), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(Psns.Common.Mvc.ViewBuilding.Activation.App_Start.ViewBuildingActivator), "Stop")]
+
+namespace Psns.Common.Mvc.ViewBuilding.Activation.App_Start
+{
+    using System.Web.Http;
+
+    using Newtonsoft.Json.Serialization;
+
+    public static class ViewBuildingActivator
+    {
+        /// <summary>
+        /// Registers the ViewBuilding WebApiConfig, Adds ViewBuilding Controllers namespace to Controller Builder
+        /// and sets Json Serialization to use CamelCasePropertyNamesContractResolver.
+        /// </summary>
+        public static void Start()
+        {
+            Psns.Common.Mvc.ViewBuilding.Infrastructure.WebApiConfig.Register(GlobalConfiguration.Configuration);
+            
+            System.Web.Mvc.ControllerBuilder.Current.DefaultNamespaces.Add("Psns.Common.Mvc.ViewBuilding.Controllers");
+
+            GlobalConfiguration.Configuration
+                .Formatters
+                .JsonFormatter
+                .SerializerSettings
+                .ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+
+        /// <summary>
+        /// Stops the application.
+        /// </summary>
+        public static void Stop()
+        {
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Psns Common Mvc ViewBuilding Activation [![NuGet Version](http://img.shields.io/nuget/v/Psns.Common.Mvc.ViewBuilding.Activation.svg?style=flat)](https://www.nuget.org/packages/Psns.Common.Mvc.ViewBuilding.Activation/) [![NuGet Downloads](http://img.shields.io/nuget/dt/Psns.Common.Mvc.ViewBuilding.Activation.svg?style=flat)](https://www.nuget.org/packages/Psns.Common.Mvc.ViewBuilding.Activation/)
+
+Handles activation and registration of view building components at Mvc App start up
+
+## CI build status
+[![Build Status](https://www.myget.org/BuildSource/Badge/psns-common?identifier=a5a3f8bb-6b73-443f-bb02-7bd8f04ff3f9)](https://www.myget.org/)
